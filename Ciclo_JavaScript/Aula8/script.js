62e1782cef8eace160b683074f07817a504fae7d
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-07-20 13:45:30 -0300

    Add files via upload

---
+++ Ciclo_JavaScript/Aula8/script.js
@@ -0,0 +1,22 @@
+//Criando uma Array
+const frutas = ["banana", "maçã", "uva", "abacate"];
+
+
+//Acessando os itens de uma Array
+console.log(frutas[0]);
+console.log(frutas[1]);
+console.log(frutas[2]);
+console.log(frutas[3]);
+
+
+//Array dentro de uma outra Array
+const perfil = [
+    "Izabela",
+    23,
+    1.70,
+    pets = ["cachorro", "gato"]
+]
+
+
+//Acessando um elemento de uma Array que está dentro de outro Array
+console.log(perfil[3][1]);
