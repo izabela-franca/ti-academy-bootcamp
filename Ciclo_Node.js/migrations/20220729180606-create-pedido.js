b4b20df0ac6081a6a890582e0362801f8364dca3
Izabela França <ifranca.sc@gmail.com>
2022-07-30 10:47:01 -0300

    Inserindo novos  arquivos

---
+++ Ciclo_Node.js/migrations/20220729180606-create-pedido.js
@@ -0,0 +1,40 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Pedidos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      data: {
+        type: Sequelize.DATEONLY
+      },
+      ClienteId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        references:{
+          model: 'clientes',
+          key: 'id'
+        } ,
+        OnUpdate: 'CASCADE',
+        OnDelete: 'CASCADE'
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Pedidos');
+  }
+};
