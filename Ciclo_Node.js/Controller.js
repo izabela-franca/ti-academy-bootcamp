15ea86788da381692704b624efd1026d64670a63
Izabela França <ifranca.sc@gmail.com>
2022-08-02 11:06:09 -0300

    Novos registros inseridos

--- Ciclo_Node.js/Controller.js
+++ Ciclo_Node.js/Controller.js
@@ -1,8 +1,9 @@
-/*--------------Implementando inserções--------------*/
+/*--------------Criando a camada de Controller--------------*/
 
 //Importando os módulos 
 const express = require ('express');
 const cors =  require ('cors');
+const models = require('./models'); 
 
 
 //Iniciando a criação do Controller 
@@ -10,22 +11,65 @@ const app = express();
 app.use(cors());
 
 
-//Exibindo mensagems
+//Criando variáveis para se associarem a cada classe
+let cliente = models.Cliente;
+let itempedido = models.ItemPedido;
+let pedido = models.Pedido;
+let servico = models.Servico;
+
+
+//Definindo rotas e configurando mensagens
 app.get('/', function(req, res){
     res.send('Olá, mundo!')
 });
 
-app.get('/clientes', function(req, res){
-    res.send('Seja bem-vindo(a) a ServicesTI.')
+
+//Definindo rotas e inserindo novos registros
+app.get('/servicos', async(req, res) => {
+    await servico.create({
+        nome: "Node.js",
+        descricao: "Desenvolvimento de aplicação back-end",
+        createAt: new Date(),
+        updateAt: new Date()
+    });
+    res.send('Serviço criado com sucesso!')
 });
 
-app.get('/servicos', function(req, res){
-    res.send('Veja nossos serviços disponíveis.')
+app.get('/clientes', async(req, res) => {
+    await cliente.create({
+        nome: "Izabela França",
+        endereco: "Rua Marechal Deodoro",
+        cidade: "São Carlos",
+        uf: "SP",
+        nascimento: 03/05/1991,
+        clienteDesde: 15/02/2021,
+        createdAt: new Date(),
+        updatedAt: new Date()
+    });
+    res.send('Cliente criado com sucesso.')
 });
 
-app.get('/pedidos', function(req, res){
-    res.send('Pedidos: ')
-})
+app.get('/pedidos', async(req, res) => {
+    await pedido.create({
+        data: new Date(),
+        createAt: new Date(),
+        ClienteId: 1,
+        updateAt: new Date()
+    });
+    res.send('Pedido criado com sucesso!')
+});
+
+/*
+app.get('/itempedidos', async(req, res) => {
+    await itempedido.create({
+        ServicoId: 1,
+        quantidade: 1,
+        valor: 299.90,
+        createAt: new Date(),
+        updateAt: new Date()
+    });
+    res.send('Item pedido criado com sucesso!')
+});*/
 
 
 //Criando uma porta de acesso
@@ -34,5 +78,7 @@ let port = process.env.PORT || 3001;
 app.listen(port, (req, res)=>{
     console.log('Servidor ativo: http://localhost:3001')
 });
+
+
 
 
