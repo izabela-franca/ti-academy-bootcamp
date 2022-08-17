223b18a31478f6a5450e087176be2ace747f3bc8
Izabela França <ifranca.sc@gmail.com>
2022-08-17 07:43:59 -0300

    Criando tabelas

--- Ciclo_React/src/views/Servico/Listar/index.js
+++ Ciclo_React/src/views/Servico/Listar/index.js
@@ -1,7 +1,41 @@
+import { Container, Table } from "reactstrap";
+
 export const ListarServicos = () => {
-    return(
-        <div>
-            <h1>Lista de Serviços</h1>
-        </div>
-    )
-}
+  return (
+    <div>
+      <Container>
+        <h1>Visualizar informações do serviço</h1>
+        <Table striped>
+          <thead>
+            <tr>
+              <th>ID</th>
+              <th>Nome</th>
+              <th>Descrição</th>
+              <th>Ação</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>1</th>
+              <td>Mark</td>
+              <td>Otto</td>
+              <td>@mdo</td>
+            </tr>
+            <tr>
+              <th>2</th>
+              <td>Jacob</td>
+              <td>Thornton</td>
+              <td>@fat</td>
+            </tr>
+            <tr>
+              <th>3</th>
+              <td>Larry</td>
+              <td>the Bird</td>
+              <td>@twitter</td>
+            </tr>
+          </tbody>
+        </Table>
+      </Container>
+    </div>
+  );
+};
