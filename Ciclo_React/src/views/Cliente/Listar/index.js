2acb1f2062c092d97e62021a7cd2743df0e00416
Izabela França <101933646+izabela-franca@users.noreply.github.com>
2022-08-23 10:43:00 -0300

    Update index.js

--- Ciclo_React/src/views/Cliente/Listar/index.js
+++ Ciclo_React/src/views/Cliente/Listar/index.js
@@ -1,4 +1,30 @@
+import {useEffect, useState} from 'react'
+import {axios} from 'axios'
+import {api} from '../../../config/index'
+
 export const ListarClientes = () => {
+    const [data, setData] = useState([])
+    const [status, setStatus] = useState({
+        type: '',
+        message: ''
+    })
+
+    const getClientes = async () => {
+        await axios.get(api+"/listaclientes")
+        .then((response) => {
+            console.log(response.data.cli)
+            setData(response.data.cli)
+        })
+        .catch(()=> {
+            console.log("Erro: sem conexão com a API.")
+        })
+    };
+
+    //Chamando função getClientes
+    useEffect(()=> {
+        getClientes();
+    }, [])
+
     return(
         <div>
             <h1>Lista de Clientes</h1>
